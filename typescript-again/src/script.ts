cd3eeffc267a7806cb21780a9d72613253baa788
arnabw <arnabsahawrk@gmail.com>
2024-07-28 02:26:55 +0600

    explicit & union type

--- typescript-again/src/script.ts
+++ typescript-again/src/script.ts
@@ -1,2 +1,27 @@
 const country = "I love Bangladesh";
 console.log(country);
+
+let a: (string | number)[] = [];
+a.push("Arnab", 99);
+
+console.log(a);
+
+let c: {
+  name: string;
+  age: number;
+  adult: boolean;
+};
+
+c = {
+  name: "Arnab Saha",
+  age: 25,
+  adult: true,
+};
+
+console.log(c);
+
+let d: object;
+
+d = [5, 6, 7];
+
+console.log(d);
